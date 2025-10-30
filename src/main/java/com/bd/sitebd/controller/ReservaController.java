464c4e81e4b8dce0e9dbde6880a698891aa3b21e
Diego Guaglianone <diegofguaglianone@gmail.com>
2025-10-30 14:47:18 -0300

    mudancas adicionar-sala

--- src/main/java/com/bd/sitebd/controller/ReservaController.java
+++ src/main/java/com/bd/sitebd/controller/ReservaController.java
@@ -1,12 +1,10 @@
 package com.bd.sitebd.controller;
 
 import com.bd.sitebd.model.Reserva;
-import com.bd.sitebd.model.enums.StatusReserva;
 import com.bd.sitebd.service.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
